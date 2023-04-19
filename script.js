f013a0b79ff6821536b1a3f397fb9f18caa62d25
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-19 21:51:59 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document
+  .querySelector(".TINO_onlineNewspaper")
+  .addEventListener("click", function () {
+    window.location.href = "http://v1238.ndsoftnews.net/index2.html";
+  });
+document
+  .querySelector(".TINO_instagram")
+  .addEventListener("click", function () {
+    window.location.href = "https://www.instagram.com/tu__times/";
+  });
+document
+  .querySelector(".TINO_kakaoChannel")
+  .addEventListener("click", function () {
+    window.location.href = "http://pf.kakao.com/_xmXxbmxj";
+  });
+document
+  .querySelector(".TINO_naverCafe")
+  .addEventListener("click", function () {
+    window.location.href = "https://cafe.naver.com/thekputimes";
+  });
